48b71e5c04ca9dafdc462206192e3d788b14c82b
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2020-10-30 20:26:09 -0400

    working on fuction

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,19 @@
 // Assignment code here
 
-var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-var numbers = "123456789"
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowercaseLetters = "abcdefghijklmnop"
-
-var includedCharacters = ''
 
 // ask the user how many characters they would like their password to contain
 var getPassowrdLength = function (){
+  
   var length = window.prompt("How many characters would you like your password to contain?");
-  length = parseInt (length);
-  console.log( typeof parseInt (length));
   if (length < 8) {
-    window.alert ("Your password must me as least 8 characters. Please try again.");
+    window.alert ("Your password must be as least 8 characters. Please try again.");
     return getPassowrdLength();
   }
   if (length > 128) {
     window.alert("Your password must be less than 129 characters. Please try again.");
     return getPassowrdLength();
   }
-  if (typeof length === 'string'){
+  if (typeof length === "string"){
     window.alert("Password length must be provided as a number. Please try again");
     return getPassowrdLength();
   }
@@ -31,48 +24,75 @@ var getPassowrdLength = function (){
     
 };
 
-getPassowrdLength();
+
+//getPassowrdLength();
+
+
+var generatePassword = function(){ 
+
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numbers = "123456789";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercaseLetters = "abcdefghijklmnop";
+
+var selectedCharacters = '';
 
 // // ask user if they want to include special characters 
  var includeSpecialCharacters = window.confirm("Click OK to confirm including special characters.");
-  if(includeSpecialCharacters) {
-    //then special characters should take on the value of this string array [ " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+  if(includeSpecialCharacters) { selectedCharacters += specialCharacters 
   }
-  else {
-    //special characters will not be included in password generator function
-  }
-
-
-
+  console.log(selectedCharacters);
+  
   // ask the user if they want to include numeric characters 
   var includeNumbers = window.confirm("Click OK to confirm including numeric characters.");
   if (includeNumbers) {
-    // then include numeric characters in password generation 
+    selectedCharacters += numbers
   }
-  else {
-    //do not incude numeric characters in password generation 
-  }
-
-
+  console.log(selectedCharacters);
+  
 
   // ask the user if they want to include lowercase letters 
   var includeLowercase = window.confirm("Click OK to confirm including lowercase letters.");
-  if(includeLowercase){
-    //then include lowercase letters in password generation
+  if(includeLowercase){ 
+    selectedCharacters += lowercaseLetters
   }
-  else {
-    //do not include lowercase letters in password generation 
-  }
-
-
+  console.log(selectedCharacters);
+  
   //as the user if they want to include uppercase letters 
   var includeUppercase = window.confirm("Click OK to confirm including uppercase letters.");
-  if (includeUppercase) {
-    // then include uppercase letters in password generation
+  if (includeUppercase) { 
+    selectedCharacters += uppercaseLetters 
   }
-  else {
-    // do not include uppercase letters in password generation
+  console.log(selectedCharacters); 
+  console.log(selectedCharacters.length); 
+
+  if (selectedCharacters === null || selectedCharacters === "") {
+    window.alert("You must select at least one character type. Please try again.");
+    return generatePassword();
   }
+
+  // loop through selectedCharacters string as many times as password length 
+  for (i = 0; i<length; i++) {
+
+    
+  //str.charAt(Math.floor(Math.random() * str.length));
+  // var lengthOfPassword = 60;
+//   var lengthOfCharArray = 81;
+//   for(var i = 0; i < lengthOfPassword; i++) {
+//   console.log(Math.floor(Math.random() * lengthOfCharArray) + 1);
+
+// var lengthOfPassword = 60;
+// var lengthOfCharArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// for(var i = 0; i < lengthOfPassword; i++) {
+//   console.log(lengthOfCharArray.charAt(Math.floor(Math.random() * lengthOfCharArray.length)));
+// }
+// }
+  }
+
+};
+generatePassword();
+
+  
 
 
 
